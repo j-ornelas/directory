64bf59c2f07faec3648cf6fdea1f47ff23e37006
john ornelas <johnnyoh1988@gmail.com>
2019-06-26 16:40:17 -0700

    feat(redux) add redux to App

--- application/src/components/App.tsx
+++ application/src/components/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Home } from './';
+import { setAllEmployees } from '../redux/actions';
 
-export class App extends React.Component {
+interface AppProps {
+  setAllEmployees:Function;
+}
+class AppComponent extends React.Component<AppProps> {
+  state = {
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    fetch('/employees')
+      .then(res => res.json())
+      .then(info => {
+        this.props.setAllEmployees(info.employees);
+        this.setState({ isLoading: false });
+      })
+  }
+
   render() {
-    return (
-      <div>
-        App Component.
-      </div>
+    return this.state.isLoading ? <p>LOADING....</p> : (
+      <Home />
     )
   }
 }
+
+export const App = connect(null, { setAllEmployees })(AppComponent);
