17603182c28420dfcbf77c32539e82d286bb3c4d
john ornelas <johnnyoh1988@gmail.com>
2019-06-26 15:29:06 -0700

    chore(server) add comment to employee route

--- server/build/routes/employees.js
+++ server/build/routes/employees.js
@@ -8,6 +8,8 @@ var node_fetch_1 = __importDefault(require("node-fetch"));
 var router = express_1.Router();
 exports.router = router;
 router.get('/employees', function (req, res) {
+    // Normally, we'd be pulling this data from a DB here
+    // instead of getting it from a randomizer API.
     var numOfResults = req.body.numOfResults;
     node_fetch_1.default("https://randomuser.me/api/?results=" + (numOfResults || 10))
         .then(function (info) { return info.json(); })
--- server/src/routes/employees.ts
+++ server/src/routes/employees.ts
@@ -7,6 +7,8 @@ interface RequestWithBody extends Request {
   body: { [key:string]: string|undefined };
 }
 router.get('/employees', (req:RequestWithBody, res:Response) => {
+  // Normally, we'd be pulling this data from a DB here
+  // instead of getting it from a randomizer API.
   const { numOfResults } = req.body;
   fetch(`https://randomuser.me/api/?results=${numOfResults || 10}`)
     .then(info => info.json())
