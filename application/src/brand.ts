dea298b88e7716c878afd9e1d4ed6e63ab90d82e
john ornelas <johnnyoh1988@gmail.com>
2019-06-27 16:38:05 -0700

    refac(brand) use global brand object for fonts / colors

---
+++ application/src/brand.ts
@@ -0,0 +1,16 @@
+interface ColorsInterface {
+  WHITE:string;
+  WARNING:string;
+  GRAY:string
+}
+interface FontsInterface {
+  SANS_SERIF:string;
+}
+export const colors:ColorsInterface = {
+  WHITE: '#FFFFFF',
+  WARNING: '#FF6347',
+  GRAY: '#D3D3D3',
+}
+export const fonts:FontsInterface = {
+  SANS_SERIF: 'sans-serif',
+}
